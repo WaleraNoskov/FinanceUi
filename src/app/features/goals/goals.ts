54c4da8d33712071ee5f1f3fc56b2862af205869
Valery Noskov <rahdah06@gmail.com>
2025-06-24 16:48:18 +0300

    feat: stylised list and button, but overflow issue.

--- src/app/features/goals/goals.ts
+++ src/app/features/goals/goals.ts
@@ -1,24 +1,47 @@
 import {Component, effect} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {GoalStore} from './goals-store';
+import {MatButton, MatFabButton, MatIconButton} from "@angular/material/button";
+import {
+  MatList,
+  MatListItem,
+  MatListItemIcon,
+  MatListItemLine,
+  MatListItemMeta,
+  MatListItemTitle
+} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
+import {MatTooltip} from "@angular/material/tooltip";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   standalone: true,
   selector: 'app-goals',
-  imports: [CommonModule],
+  imports: [CommonModule, MatButton, MatList, MatListItem, MatListItemTitle, MatListItemLine, MatListItemMeta, MatIconButton, MatIcon, MatFabButton],
   template: `
-    <h2>Мои цели</h2>
+      <div class="goal-container">
+          <div>
+              <button class="fab-button" matFab (click)="create()">
+                  <mat-icon>add</mat-icon>
+              </button>
 
-    <button (click)="create()">add</button>
-
-    <div *ngIf="store.isLoading()">Загрузка...</div>
-
-    <ul>
-      <li *ngFor="let goal of store.goalList()">
-        {{ goal.title }} — {{ goal.currentAmount }} / {{ goal.targetAmount }}
-        <button (click)="delete(goal.id)">Удалить</button>
-      </li>
-    </ul>
+              <div class="goal-list-wrapper">
+                  <mat-list>
+                      @for (goal of store.goalList(); track goal.id) {
+                          <mat-list-item>
+                              <span matListItemTitle>{{ goal.title }}</span>
+                              <span matListItemLine>{{ goal.currentAmount }} of {{ goal.targetAmount }}</span>
+                              <div matListItemMeta>
+                                  <button matIconButton aria-label="Delete goal" (click)="delete(goal.id)">
+                                      <mat-icon>delete</mat-icon>
+                                  </button>
+                              </div>
+                          </mat-list-item>
+                      }
+                  </mat-list>
+              </div>
+          </div>
+      </div>
   `,
   styleUrl: './goals.scss'
 })
