26183347e43f796e6836a22e7e3ca12c8cbd864c
Valery Noskov <rahdah06@gmail.com>
2025-07-01 16:57:49 +0300

    fix: semi done goals page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes } from '@angular/router';
 import {BoardsManagementWidget} from './features/boards/widgets/boards-management-widget/boards-management-widget';
 import {GoalsManagementWidget} from './features/goals/widgets/goals-management-widget/goals-management-widget.component';
 import {SettingsPage} from './pages/settings/settings-page/settings-page';
+import {GoalsPage} from './pages/Goals/goals-page/goals-page';
 
 export const routes: Routes = [
-  {path: 'boards', component: BoardsManagementWidget},
-  {path: 'goals', component: GoalsManagementWidget},
+  {path: 'goals', component: GoalsPage},
   {path: 'settings', component: SettingsPage}
 ];
---
+++ src/app/pages/Goals/goals-page/goals-page.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
+import {
+  GoalsManagementWidget
+} from '../../../features/goals/widgets/goals-management-widget/goals-management-widget.component';
+
+@Component({
+  selector: 'app-goals-page',
+  imports: [
+    MatToolbar,
+    MatToolbarRow,
+    GoalsManagementWidget
+  ],
+  template: `
+    <div class="goals-page">
+      <div class="header-container">
+        <mat-toolbar color="primary" class="header">
+          <mat-toolbar-row>
+            <h1 class="header-title">
+              Goals
+            </h1>
+          </mat-toolbar-row>
+          <mat-toolbar-row>
+          <span class="header-subtitle">
+            Review and edit your goals.
+          </span>
+          </mat-toolbar-row>
+        </mat-toolbar>
+      </div>
+
+      <div class="content-container">
+        <app-goals-management-widget/>
+      </div>
+    </div>
+  `,
+  styles: `
+    .goals-page {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+
+    .header-container {
+      padding: 8px;
+    }
+
+    .header {
+      border-radius: 24px;
+      padding: 24px;
+      gap: 8px;
+    }
+
+    .header-title {
+      font-size: 48px;
+    }
+
+    .header-subtitle {
+      font-size: 16px;
+      text-wrap: auto;
+    }
+
+    .content-container {
+      flex-grow: 1;
+      overflow-y: auto;
+      padding: 0 24px;
+    }`
+})
+export class GoalsPage {
+
+}
